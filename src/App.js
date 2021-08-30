a8bc24f23b6711ea3dfff7a346f16320898651fe
Branimir026 <br.belic@gmail.com>
2021-08-30 16:14:27 +0200

    App broken down to Navigation and SearchForm components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
+import Navigation from "./Components/Navigation";
+import SearchForm from "./Components/SearchForm";
 
 function App() {
   return (
     <>
-      <Navbar className="bg-primary navbar-dark mb-5">
-        <Container>
-          <Navbar.Brand>Reddit search client</Navbar.Brand>
-        </Container>
-      </Navbar>
+      <Navigation />
+      <SearchForm />
     </>
   );
 }
